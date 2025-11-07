3fd2011274f5a42bd455e889ebbf93a8766a7578
ENZHOUHUY <1123150029@global.ac.id>
2025-11-07 13:37:22 +0700

    membuat row navigasi

--- lib/screen/splash1.dart
+++ lib/screen/splash1.dart
@@ -42,6 +42,39 @@ class SplashScreen1 extends StatelessWidget {
                       color: Colors.red
                     ),
                   ),
+                  SizedBox(height: 30,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 10,
+                        height: 10,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.red
+                        ),
+                      ),
+                      SizedBox(width: 10,),
+                      Container(
+                        width: 10,
+                        height: 10,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.grey
+                        ),
+                      ),
+                      SizedBox(width: 10,),
+                      Container(
+                        width: 10,
+                        height: 10,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.grey
+                        ),
+                      ),
+                      SizedBox(width: 10,),
+                    ],
+                  ),
                 ],
               ),
             ),
